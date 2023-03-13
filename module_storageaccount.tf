58ca27dcefd712fbfde161368bf77907059c84ad
arestrepo94 <126503930+arestrepo94@users.noreply.github.com>
2023-03-13 12:44:36 -0500

    Update module_storageaccount.tf

--- module_storageaccount.tf
+++ module_storageaccount.tf
@@ -1,11 +1,14 @@
+#---------------------------------------------------------
+# Storage Account Creation 
+#----------------------------------------------------------
 resource "azurerm_storage_account" "stac" {
 	name						= var.name
 	location					= var.location
 	resource_group_name			= var.resource_group_name
 	account_kind				= var.account_kind
-	account_tier				= local.account_tier
-	account_replication_type	= local.account_replication_type
+	account_tier				= var.account_tier
 	access_tier					= var.access_tier
+	account_replication_type	= var.account_replication_type
 	enable_https_traffic_only	= true
 	tags		= {
         environment				= var.tag_environment
@@ -17,4 +20,37 @@ resource "azurerm_storage_account" "stac" {
 		cost					= var.tag_cost
 		name_service			= var.tag_name_service
 		}
+dynamic "network_rules" {
+    for_each = var.network_rules != null ? ["true"] : []
+    content {
+      default_action             = "Deny"
+      bypass                     = var.network_rules.bypass
+      ip_rules                   = var.network_rules.ip_rules
+      virtual_network_subnet_ids = var.network_rules.subnet_ids
+    }
+  }
+ dynamic "static_website" {
+	for_each = local.if_static_website_enabled
+		content {
+		index_document     = var.index_path
+		error_404_document = var.custom_404_path
+    }
+  }  
+}
+#-------------------------------
+# Storage Container Creation
+#-------------------------------
+resource "azurerm_storage_container" "container" {
+  count                 = length(var.containers_list)
+  name                  = var.containers_list[count.index].name
+  storage_account_name  = azurerm_storage_account.stac.name
+  container_access_type = var.containers_list[count.index].access_type
+}
+#-------------------------------
+# Storage Tables Creation
+#-------------------------------
+resource "azurerm_storage_table" "tables" {
+  count                = length(var.tables)
+  name                 = var.tables[count.index]
+  storage_account_name = azurerm_storage_account.stac.name
 }
